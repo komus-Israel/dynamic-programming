62c72b82a0ec021e21d79977a26a7978b1ea3478
komus-Israel <komolehinIsrael@gmail.com>
2021-12-29 17:25:01 +0100

    the binary starts and ends approprately

--- decimalToBinary.js
+++ decimalToBinary.js
@@ -2,12 +2,12 @@ function decimalToBinary(decimal, result="") {
 
     if(decimal <= 1) {
         remainder = 1
-        result += remainder
+        result = remainder + result
         return result
     }
 
     remainder = decimal % 2
-    result += remainder
+    result = remainder + result
     return decimalToBinary(Math.floor(decimal/2), result)
 
 }
