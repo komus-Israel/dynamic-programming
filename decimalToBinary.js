32719fc6bf692e1d473c3ee7b05b6dafea98af32
komus-Israel <komolehinIsrael@gmail.com>
2021-12-29 17:18:48 +0100

    used Math.floor method to dicide decimals

--- decimalToBinary.js
+++ decimalToBinary.js
@@ -1,5 +1,16 @@
-function decimalToBinary(decimal) {
+function decimalToBinary(decimal, result="") {
 
-    console.log(decimal.toString())
+    if(decimal <= 1) {
+        remainder = 1
+        result += remainder
+        return result
+    }
+
+    remainder = decimal % 2
+    result += remainder
+    return decimalToBinary(Math.floor(decimal/2), result)
 
 }
+
+console.log(decimalToBinary(64))
+
