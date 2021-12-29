d804196d1ab242af0a0acdb4042e1a6ca360af32
komus-Israel <komolehinIsrael@gmail.com>
2021-12-29 21:04:13 +0100

    abbreviation recursive function complete

--- abbreviation.js
+++ abbreviation.js
@@ -1,9 +1,28 @@
-function abbreviation(a, b) {
+function abbreviation(a, b, position, result="") {
     if (a === b) {
         return "YES"
     }
 
+    if (result === b) {
+        return "YES"
+    }
 
+    a = a.toUpperCase()
+
+   
+    if (position < a.length) {
+        if (b.includes(a[position])) {
+            result = result + a[position]
+        }
+        return abbreviation(a, b, position + 1, result) 
+    }
+
+    return 'NO'
+        
+    
+
+    
 }
+   
 
-console.log(abbreviation("ABCD", "ABCD"))
+console.log(abbreviation("aBCdEfgh", "RTABCDYU", 0))
