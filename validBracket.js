6c041d6d345e5c200ed2beed68d5ee34e55a99ce
komus-Israel <komolehinIsrael@gmail.com>
2021-12-30 20:12:27 +0100

    added callback to the seed exchange

--- validBracket.js
+++ validBracket.js
@@ -9,11 +9,36 @@ function isValid(string, position, result={}) {
     if (string.length % 2 != 0) {
         return false
     }
+
+    if(position <= string.length) {
+
+        if(string[position + 1] === brackets[string[position]]) {
+
+            if(result[true]) {
+                result[true] += 1
+            } else {
+                result[true] = 1
+            }
+                      
+        } else {
+            if(result[false]) {
+                result[false] += 1
+            } else {
+                result[false] = 1
+            }  
+        }
+    
+        isValid(string, position + 2, result)
+
+        return result
+    }
+
+    
     
     
 }
 
 
-//console.log(isValid('()', 0))
+console.log(isValid('()(){}', 0))
 //console.log(isValid('{}{}(){}{}', 0))
-console.log(isValid('({{()}})', 0))
+//console.log(isValid('({{()}})', 0))
