5b2b3b4d0a56457eaf3156ba6ac8402fdb002630
komus-Israel <komolehinIsrael@gmail.com>
2021-12-30 12:03:20 +0100

    introduced a base case

--- validBracket.js
+++ validBracket.js
@@ -6,48 +6,14 @@ function isValid(string, position, result={}) {
         '(' : ')'
     }
 
-    if (string.length % 2 == 1) {
+    if (string.length % 2 != 0) {
         return false
     }
     
-
-    if (position <= string.length) {
-        if (string[position + 1] === brackets[string[position]]) {
-    
-            if(result[true]) {
-                result[true] += 1
-            } else {
-                result[true] = 1
-            }
-
-            
-            
-        } else {
-
-            if(result[false]) {
-                result[false] += 1
-            } else {
-                result[false] = 1
-            }
-            
-        }
-
-        isValid(string, position + 2, result)
-        
-        if (false in result) {
-            return false
-        }
-
-        return true
-    }
-   
-    
-
-
     
 }
 
 
 //console.log(isValid('()', 0))
-console.log(isValid('{}{}(){}{{', 0))
-console.log(isValid('{({()})}', 0))
+//console.log(isValid('{}{}(){}{}', 0))
+console.log(isValid('({{()}})', 0))
