48ecef2d239444f606f651a1e1d1124d7a611c36
komus-Israel <komolehinIsrael@gmail.com>
2021-12-31 19:47:28 +0100

    was able to get the unique values

--- firstUnique.js
+++ firstUnique.js
@@ -1,8 +1,24 @@
-function firstUnique(array, memo={}) {
+function firstUnique(array, position, memo={}) {
 
     // return the only elemnt present in the array
-    if (array.length === 0) {
+    if (array.length === 1) {
         return array[0]
+    }
+
+    if (position < array.length) {
+
+        if (memo[array[position]]) {
+
+            memo[array[position]] = false
+        } else {
+            memo[array[position]] = position
+        }   
+
+       
+
+        firstUnique(array, position + 1, memo)
+
+        return memo
     }
 
     
@@ -10,5 +26,5 @@ function firstUnique(array, memo={}) {
 }
 
 
-array = [10]
-console.log(firstUnique(array))
+array = [10, 20, 50, 50, 30]
+console.log(firstUnique(array, 0))
