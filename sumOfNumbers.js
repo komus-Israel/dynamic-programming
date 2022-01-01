75a3240bc5357f55ecb1dc59bdcd3aabfa91cd76
komus-Israel <komolehinIsrael@gmail.com>
2022-01-01 19:43:04 +0100

    optimized the sum by memoization

--- sumOfNumbers.js
+++ sumOfNumbers.js
@@ -1,3 +1,15 @@
-function sum() {
-    
+function sum(number, memo={}) {
+    if(number <= 1) {
+        return number
+    }
+
+    if (number in memo) {
+        return memo[number]
+    }
+
+    memo[number] = number + sum(number - 1, memo)
+    return memo[number]
 }
+
+console.log(sum(10000)) //5050
+console.log(sum(50)) // 1275
