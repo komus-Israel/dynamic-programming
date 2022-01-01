b924936fbfe170e1e5276614765e47fc40ae2659
komus-Israel <komolehinIsrael@gmail.com>
2022-01-01 01:07:07 +0100

    handled the first kind of valid brackets

--- validBracket.js
+++ validBracket.js
@@ -10,6 +10,28 @@ function isValid(string, position) {
     if (string.length === 0) {
         return true
     }
+
+    if (string.length % 2 != 0) {
+        return false
+    }
+
+    splittedString = string.split('')
+
+   if ( splittedString[position + 1] === brackets[splittedString[position]]) {
+
+        splittedString.pop()
+        splittedString.pop()
+
+        string = splittedString.join('')
+        return isValid(string, position + 2)
+        
+   }
+
+   
+
+   
+
+
 }
 
-console.log(isValid('(){}()(){}))))))', 0))
+console.log(isValid('(){}()[]]', 0))
